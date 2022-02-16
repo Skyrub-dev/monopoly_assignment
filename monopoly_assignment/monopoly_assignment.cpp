13dd0327026e253c739575e9b54c50469c5cae39
Tom <tom@PC.com>
2022-02-16 12:31:57 +0000

    init

--- monopoly_assignment/monopoly_assignment.cpp
+++ monopoly_assignment/monopoly_assignment.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+//NEED TO FIRST OF ALL READ IN 'MONOPOLY.txt' WHICH SHOULD CONTAIN ALL THE PROPERTY DETAILS
+//PUT THIS IN A HEADER FILE AND FIND OUT HOW TO LINK THEM
+
+class player
+{
+public:
+    int cash = 0;
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Welcome to monopol-ish!\n";
 }
