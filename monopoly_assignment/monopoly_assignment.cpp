e0d0bce1245a6af6c61b1b6b717352174867572c
Tom <tom@PC.com>
2022-04-20 08:17:47 +0100

    commit

--- monopoly_assignment/monopoly_assignment.cpp
+++ monopoly_assignment/monopoly_assignment.cpp
@@ -1,24 +1,80 @@
 #include <iostream>
+#include <iterator>
 #include <fstream>
+#include <map>
+//#include "random.h"
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <memory>
 
 //NEED TO FIRST OF ALL READ IN 'MONOPOLY.txt' WHICH SHOULD CONTAIN ALL THE PROPERTY DETAILS
 //PUT THIS IN A HEADER FILE AND FIND OUT HOW TO LINK THEM
 
-const char POUND = 156;
+//const char POUND = 156;
 
 class CSquare //square properties
 {
 public:
+    //CSquare(std::vector<CSquare*>&);
+    
     int squarenum;
+    std::string squarename;
     int cost;
     int colourgroup;
+
+    void setCSquareName(std::string);
+    void setCSquarenum(int);
+    //up for removal if can't get it working
+    void setCSquarecost(int);
+    void setCSquarecolour(int);
+
+    std::string getCSquareName() const;
+    int getCSquarenum() const;
+    int getCSquarecolour() const;
+
 };
+
+void CSquare::setCSquareName(std::string CSquarename)
+{
+    squarename = CSquarename;
+}
+
+void CSquare::setCSquarenum(int CSquarenum)
+{
+    squarenum = CSquarenum;
+}
+
+void CSquare::setCSquarecost(int CSquarecost)
+{
+    cost = CSquarecost;
+}
+
+void CSquare::setCSquarecolour(int CSquarecolour)
+{
+    colourgroup = CSquarecolour;
+}
+
+std::string CSquare::getCSquareName() const
+{
+    return squarename;
+}
+
+int CSquare::getCSquarenum() const
+{
+    return squarenum;
+}
+
+int CSquare::getCSquarecolour() const
+{
+    return colourgroup;
+}
 
 class player //player properties
 {
 public:
     std::string name;
-    int cash = 0;
+    int cash;
     int position;
 };
 
@@ -26,7 +82,59 @@ int main()
 {
     player player1;
     player player2;
-    std::cout << "Welcome to monopol-ish!\n";
 
-    //std::ifstream monopoly("monopoly.txt", fstream::in);
+    
+    int i = 0;
+    std::string line[26];
+    std::string Squarename;
+    std::cout << "Welcome to monopol-ish!\n";
+    const int MAX_SIZE = 26;
+    std::ifstream monopoly("Monopoly.txt");
+
+    std::getline(monopoly, line[i]);
+
+    std::vector<CSquare*> CSquareVector;
+    for (int i = 0; i < MAX_SIZE; i++)
+    {
+        
+
+        while (i < MAX_SIZE && monopoly >> Squarename)
+        {
+            
+        }
+    }
+    
+    //vector_of_pointers.push_back(line[i]);
+
+    while (std::getline(monopoly, line))
+    {
+        vector_of_pointers.push_back(line);
+    }
+    //remember to include 'delete [] vector_of_pointers;'
+
+    //if (monopoly.is_open()) //checks if the file is open
+    //{
+    //    CSquare myArray[MAX_SIZE]; //declares the array of the object cSquare class
+
+    //    getline(monopoly, discard); //allows file to be read line by line
+    //    std::string sname; //string to store what I read in from my file
+
+    //    while (i < MAX_SIZE && monopoly >> sname) //inputs the contents of the file into the string sname
+    //    {
+    //        myArray[i].setSquareName(sname); //store the string read in into the array
+    //        std::cout << myArray[i].getSquareName(); //it outputs what is stored in the array here
+    //        i++; //adds one each time to the i counter
+    //    }
+    //}
+
+    player1.name = "Hamster";
+    player1.cash = 1500;
+    player1.position = 0;
+
+    player2.name = "Pumpkin";
+    player2.cash = 1500;
+    player2.position = 0;
+
+    
+    _CrtDumpMemoryLeaks();
 }
